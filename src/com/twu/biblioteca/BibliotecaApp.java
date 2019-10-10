490ff27b1a5ea6c361d0be5009a2675dca4a3903
Deedee <deedeedeeps@Deedees-MacBook-Pro.local>
2019-10-10 18:52:36 +1100

    Add options menu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
@@ -12,8 +13,30 @@ public class BibliotecaApp {
         String book2 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951).bookString();
         String book3 = new Book("On the Road", "Jack Kerouac", 1957).bookString();
         List<String> bookList = Arrays.asList(book1, book2, book3);
-        for (String item : bookList) {
-            System.out.println(item);
+//        to print bookList
+//        for (String item : bookList) {
+//            System.out.println(item);
+//        }
+        System.out.println("");
+        System.out.println("List of options:");
+        System.out.println("Type 0 and press enter to... if you want to quit. ");
+        System.out.println("Type 1 and press enter to... display a list of books in the library. ");
+
+        int userInput;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            userInput = sc.nextInt();
+            if (userInput == 0) {
+                System.out.println("Exiting now...");
+                break;
+            } else if (userInput != 1) {
+                System.out.println("Please select a valid option!");
+            } else {
+                System.out.println(userInput);
+                break;
+            }
         }
+
+
     }
 }
